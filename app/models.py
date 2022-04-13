f974e0f33c027c8808b4644b47327257296ce46d
LeeYoungIn <lyi@malan.kr>
2022-04-13 14:05:32 +0900

    create Account & Room Models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
+from operator import mod
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Account(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  nickname = models.CharField(max_length=30, blank=True)
+
+
+class Room(models.Model):
+  owner = models.OneToOneField(Account, on_delete=models.CASCADE)
+  mate = models.ForeignKey(Account)
